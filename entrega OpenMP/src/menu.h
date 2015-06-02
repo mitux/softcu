d637dd32cebddb65018691d12d65c07fbeccfa78
ROGER RODRIGUEZ CAMPRUBI <rrodrica20.alumnes@ub052229.mat.ub.edu>
2015-06-02 13:56:10 +0200

    OpenMP

---
+++ entrega OpenMP/src/menu.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <omp.h> 
+#include <QWidget>
+
+#include <QPushButton>
+#include <QApplication>
+#include <QGridLayout>
+#include <QFileInfo>
+#include <QLabel>
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+class QMainMenu : public QWidget
+{
+Q_OBJECT
+
+ public:
+   QMainMenu(QWidget *parent = 0);
+   int hCont = 1;
+
+   struct hist {
+     double c;
+     QString path;
+   };
+
+
+private slots:
+    void OnShowPressed();
+    QList<QString> histComp(QString);
+    static bool compare(const hist& first, const hist& second);
+    void comparaHist();
+    vector<Mat> histogram(Mat im);
+    void saveHistogram(Mat im);
+
+  private:
+    QPushButton *show_btn;
+	QPushButton *empty_btn;
+    QLabel *percentatge;
+    QLabel *accio;
+    QList<QString> fillList(QString file);
+    QString getName(QString pre, int n);
+    double mult;
+
+};
